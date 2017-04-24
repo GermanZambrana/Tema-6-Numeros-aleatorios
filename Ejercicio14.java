5f32dc8bc9af1e6b041c75c99c6e62f9bde36d6e
GermanZambrana <supergermanz@hotmail.com>
2017-04-25 00:13:20 +0200

    Numeros aleatorios lo que llevo

---
+++ Ejercicio14.java
@@ -0,0 +1,96 @@
+/*
+*
+* @author German Zambrana Ruiz
+* Ejercicio 4  - Tema 6
+*
+*/
+import java.util.Scanner;
+
+public class Ejercicio14 {
+  public static void main(String[] args) { 
+    System.out.println("Piensa un número entre 0 y el 100, el programa tratará");
+    System.out.println("de averiguar el número que has pensado en 5 intentos.");
+    System.out.println("deberás indicarle al programa si el número que él ha pensado");
+    System.out.println("es mayor o menor que el tuyo.");
+    System.out.println("______________________________________________");
+    System.out.println("Número de intentos: 5");
+    System.out.println("\nPiense el número y luego Pulse INTRO para continuar.");
+    System.console().readLine();
+    
+    int intentos = 5;
+    Scanner n = new Scanner(System.in);
+    int numero = 0; //la variable que usará el programa para intentar adivinar el número
+    boolean acertado = false;
+    String respuesta = (" "); //ésta variable almacena la respuesta del usuario
+    int rangoMinimo = 0;
+    int rangoMaximo = 100;
+    
+    
+    
+    do {
+                
+      System.out.print("¿Has pensado éste número?: ");
+      numero = (int)(Math.random() * (rangoMaximo - rangoMinimo) + rangoMinimo);
+      System.out.print(numero);
+            
+      System.out.println(); //salto de línea
+      
+      System.out.print("Responde con \"s\" o \"n\": ");
+      respuesta = n.nextLine();
+      
+      if (respuesta.equals("s")) { //pregunta si ha acertado el número
+        System.out.println("¡¡¡Ole!!!");
+        acertado = true;
+      } else {
+        System.out.println("En ese caso...");
+        System.out.print("¿Es el número que has pensado mayor? s/n: ");
+        respuesta = n.nextLine();
+      }
+      
+            
+            
+      if (respuesta.equals("s") && !acertado) {
+        rangoMinimo = numero + 1;
+        intentos--;
+        System.out.println("\nIntentos: " + intentos);
+      } else if (respuesta.equals("n") && !acertado) {
+        rangoMaximo = numero + 1;
+        intentos--;
+        System.out.println("\nIntentos: " + intentos);
+      }
+      
+         
+      
+      respuesta = (" ");
+      System.out.println("______________________________________");
+      System.out.println("\nPulse INTRO para continuar.");
+      System.console().readLine();
+    } while (intentos > 1 && !acertado);
+    
+    //////////////////////última oportunidad
+    
+    if (!acertado) {
+      numero = (int)(Math.random() * (rangoMaximo - rangoMinimo) + rangoMinimo);
+      System.out.print("\n¿Has pensado éste número?: ");
+      System.out.print(numero);
+    
+    System.out.println(); //salto de línea
+      
+      System.out.print("Responde con \"s\" o \"n\": ");
+      respuesta = n.nextLine();
+      
+      if (respuesta.equals("s")) { //pregunta si ha acertado el número
+        System.out.println("¡¡¡Ole!!!");
+        acertado = true;
+      } else {
+        intentos--;
+      }
+      
+    }
+    
+    if (intentos == 0 && !acertado) {
+      System.out.println("\nNo encontré tu número, has ganado :(");
+    }        
+        
+  }
+}
---
+++ Ejercicio18.java
@@ -0,0 +1,118 @@
+/*
+*
+* @author German Zambrana Ruiz
+* Ejercicio 18  - Tema 6
+*
+*/
+
+
+public class Ejercicio18 {
+  public static void main(String[] args) {
+    System.out.println("Vavamos a sacar 3 colores aleatorios no repetidos para");
+    System.out.println("los dormitorios de Sinestesio y Casilda.");
+    System.out.println("_____________________________________________");
+    System.out.println("Pulse INTRO para sacar 3 colores.");
+    System.console().readLine();
+        
+    
+    int a = 0; //guarda los valores de los colores para posteriormente comprobar si se repiten o no
+    int b = 0;
+    int c = 9; 
+    
+    String color1 = (" ");
+    String color2 = (" ");
+    String color3 = (" ");
+    
+    do {
+      a = (int)(Math.random()*6) + 1;
+      switch (a) { //se le asigna a "a" un color
+        case 1:
+          color1 = ("rojo");
+          break;
+          
+          case 2:
+          color1 = ("azul");
+          break;
+          
+          case 3:
+          color1 = ("verde");
+          break;
+          
+          case 4:
+          color1 = ("amarillo");
+          break;
+          
+          case 5:
+          color1 = ("violeta");
+          break;
+          
+          case 6:
+          color1 = ("naranja");
+          break;
+          
+          default:
+        } //switch colores
+        
+      b = (int)(Math.random()*6) + 1;
+      switch (b) { //se le asigna a "a" un color
+        case 1:
+          color2 = ("rojo");
+          break;
+          
+          case 2:
+          color2 = ("azul");
+          break;
+          
+          case 3:
+          color2 = ("verde");
+          break;
+          
+          case 4:
+          color2 = ("amarillo");
+          break;
+          
+          case 5:
+          color2 = ("violeta");
+          break;
+          
+          case 6:
+          color2 = ("naranja");
+          break;
+          
+          default:
+        } //switch colores
+        
+      c = (int)(Math.random()*6) + 1;
+      switch (c) { //se le asigna a "a" un color
+        case 1:
+          color3 = ("rojo");
+          break;
+          
+          case 2:
+          color3 = ("azul");
+          break;
+          
+          case 3:
+          color3 = ("verde");
+          break;
+          
+          case 4:
+          color3 = ("amarillo");
+          break;
+          
+          case 5:
+          color3 = ("violeta");
+          break;
+          
+          case 6:
+          color3 = ("naranja");
+          break;
+          
+          default:
+        } //switch colores
+    } while ((a == b) || (a == c) || (b == c));
+     
+    System.out.println("\n" + color1 + (" ") + color2 + (" ") + color3);     
+            
+  }
+}
